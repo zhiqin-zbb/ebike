59e0c1c4ae1b4f3f85e05fb3e93d0008d3465f85
451053802@qq.com <zbb19921019>
2017-11-22 22:07:41 +0800

    首页

---
+++ src/main/java/com/aaebike/controller/user/LoginController.java
@@ -0,0 +1,42 @@
+package com.aaebike.controller.user;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.aaebike.model.UserInfo;
+import com.aaebike.service.UserInfoService;
+
+/**
+ * @author zhangbinbin
+ * @version 2017/11/17
+ */
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private UserInfoService userInfoService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String showLoginPage(Model model) {
+        UserInfo userInfo = new UserInfo();
+        model.addAttribute("userInfo", userInfo);
+        return "user/login";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String login(UserInfo userInfo) {
+        Boolean validate = userInfoService.validate(userInfo);
+        if (validate) {
+            return "redirect:/result/login/successful";
+        } else {
+            return "redirect:/result/login/fail";
+        }
+    }
+}
